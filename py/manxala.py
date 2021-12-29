99e03bf009859bafadfaf3ecca274e8ccb5e06a7
J Jacob Wikner <jjwikner@gmail.com>
2021-12-29 23:16:05 +0100

    Cleaned up some functionality and rules

--- py/manxala.py
+++ py/manxala.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 import argparse
 import matplotlib.pyplot as plt
+from os import system
+import time
 
 class hole():
 
@@ -57,18 +59,17 @@ class board():
         self.verbose = verbose
 
     def hoho(self, player=0):
+        # Returns the list of gems in the first three home holes
         if player == 0:
-            return (self.holes[0].nop,self.holes[1].nop, self.holes[2].nop)
-        return (self.holes[6].nop,self.holes[7].nop, self.holes[8].nop)
+            return [self.holes[0].nop,self.holes[1].nop, self.holes[2].nop]
+        return [self.holes[6].nop,self.holes[7].nop, self.holes[8].nop]
 
     def clean(self, player=0):
         if (player == 0):
             if self.hoho(player=player) == [0,0,0]:
-                print("Clear!")
                 return True
 
         if self.hoho(player=player) == [0,0,0]:
-            print("Clear!")
             return True
 
         return False
@@ -88,6 +89,9 @@ class board():
             hole.rst()
 
     def show(self):
+        #system('clear')
+        #time.sleep(1)
+        
         print( '+-------------------------+')
         subrow = int( self.homes[0].nop / 23)
         for k in range(subrow+1):
@@ -107,29 +111,25 @@ class board():
         print( '+-------------------------+')
 
     def push(self, player=0, hole=0):
-        # if player, then rotate with 6.
-        
+        # The function that pushes the gems forward through the system.
+        # if player==1, then rotate with 6.        
+
         pearls = self.holes[hole].flush()
         hole_position = hole
+        at_base = False
+        
         for m in range(pearls):
-            hole_position = hole_position + 1  #  + m + 1
+            hole_position = hole_position + 1 
             at_base = False
-            if (player == 0):
-                if (hole_position==12):
-                    self.homes[player].add()
-                    at_base = True
-                    continue
-                if (hole_position > 12):
-                    hole_position = hole_position-13 
-            else: # (player == 1):
-                if (hole_position > 11):
-                    hole_position = hole_position-12 
-                if (hole_position == 6):
-                    self.homes[player].add() # handle this
-                    at_base = True
-                    
-                    continue
-            #print(f"------------> {hole_position}")
+            
+            if (hole_position+player*6 == 12): # This will fail as it will pass through twice
+                self.homes[player].add()
+                at_base = True
+                continue
+            
+            if (hole_position >= 12):
+                hole_position = hole_position-12
+
             self.holes[hole_position].add()
             
         return hole_position,at_base
@@ -138,50 +138,112 @@ class board():
         # ===
         gogo = True
         ctr = 0
+        base = False
+        cleaned = False
+        # rounds is for testing purposes only, to avoid deadlock in loop
+        print(hole)
+        if hole is None:
+            pearls_in_holes = self.hoho(player=player)
+            print(f"From home, starting condition { pearls_in_holes }")
+            # One of many algorithms... to be added here
+            hole = player*6 + pearls_in_holes.index(max(pearls_in_holes))
+            
+        if self.sweep(): # clean(player=0) or self.clean(player=1):
+            # Clean in home
+            # This must be done when iteration is over.
+            print(f"-------------------> Game ended! Player {player} gets the gems!")
+            gogo = False
+            cleaned = True
+            pass
+        
         while gogo:
             hole,base = self.push(player=player, hole=hole)
+            
             ctr = ctr + 1
             self.show()
             print(f"{ctr}: {hole} // {self.sum()}")
-            # if self.clean
             
-            if (player == 0) and base: # hole == 12):
-                # Then ended in home, pick random along three home holes
-                if not self.clean(player=player):
-                    mm = self.hoho(player=player)
-                    print(mm)
-                    hole = mm.index(max(mm))
-                else:
-                    print("Game ended!")
+            if base: # Ended at home base, break the loop and reiterate
+                gogo = False
+            else: # On the normal enchilada.
+                if self.holes[hole].nop == 1:                    
+                    self.holes[hole].flush()
+                    self.homes[player].add()
+                    print("Take the L! Ended on a singularity!")
+                    if (player == 0) and (hole in [0,1,2]):
+                        gems = self.holes[hole+6].nop
+                        self.holes[hole+6].flush()
+                        self.homes[player].add(nop=gems)
+                        pass
+                    if (player == 1) and (hole in [6,7,8]):
+                        gems = self.holes[hole-6].nop
+                        self.holes[hole-6].flush()
+                        self.homes[player].add(nop=gems)
+                        pass
+                    # if in home, snatch them other bitches. Wait with that.
                     gogo = False
                     
-                
-            elif (player == 1) and base: # (hole == 6):
-                # Then ended in home, pick random along three home holes
-                if not self.clean(player=player):
-                    mm = self.hoho(player=player)
-                    print("Argabarag")
-                    hole = 6+mm.index(max(mm))
-                else:
-                    print("Game ended!")
-                    gogo = False
-
-            else:
-                if self.holes[hole].nop == 1:
-                    print("Take the L! Ended on a singularity!")
-                    gogo = False
-            if ctr >= rounds:
+                # else continue as usual
+                # why is it continuing
+            if ctr >= rounds: # break check
                 gogo = False
+
                 
+        return base,cleaned # status vector, we need to add for stop here too
+
+    def sweep(self):
+        status = False
+        
+        if self.clean(player=0):
+            clean = True
+            self.homes[0].add(nop=self.holes[6].nop+self.holes[7].nop+self.holes[8].nop)
+            self.holes[6].flush()
+            self.holes[7].flush()
+            self.holes[8].flush()
+            status = True
+        if self.clean(player=1):
+            clean = True
+            self.homes[1].add(nop=self.holes[0].nop+self.holes[1].nop+self.holes[2].nop)
+            self.holes[0].flush()
+            self.holes[1].flush()
+            self.holes[2].flush()
+            status = True
+
+        return status
+        
     
 def main(args=None):
     manxala = board(verbose=True)
-    manxala.show()
-    hole = 6
-    player = 1
-    manxala.iter(player=player, hole=hole, rounds=args.rounds)
     
-    pass
+    print("==========================================================================")
+    manxala.show()
+
+    if args.hole > 11:
+        args.hole = None
+
+    assert args.player in [0,1] 
+    active_player = args.player
+    active_hole = args.hole
+
+    clean = False
+    while not clean:
+        print(f"Player {active_player} makes heris round.")
+        base,clean = manxala.iter(player=active_player, 
+                              hole=active_hole, 
+                              rounds=args.rounds)
+        active_hole = None
+
+        manxala.sweep()
+        
+        if base:
+            active_player = active_player
+        else:
+            print("Changing player!")
+            active_player = 1-active_player
+
+
+
+    manxala.show()
 
 
 if __name__ == "__main__":
@@ -194,7 +256,13 @@ if __name__ == "__main__":
     parser.add_argument("--rounds",
                         help="Number of test runs", 
                         default=3, type=int)
-    
+
+    parser.add_argument("--player",
+                        help="Player to start", 
+                        default=0, type=int)
+
+    parser.add_argument("--hole", help="Hole to start in", default = 0, type =  int)
+
     args = parser.parse_args()
     
     main(args=args)
